8819c96f75df2112a3b6348d7978cb6446985c70
Jorge Muladi <jorgeosattimuladi@gmail.com>
2024-09-24 09:10:59 +0700

    init

---
+++ AreaRectangle.ts
@@ -0,0 +1,13 @@
+function calculateArea(length: number, width: number): number {
+    return length * width;
+}
+
+// Inputs
+let l: number = 5;
+let width: number = 3;
+
+// Calculate area
+let area: number = calculateArea(l, width);
+
+// Output the result
+console.log("The area of the rectangle is:", area);
